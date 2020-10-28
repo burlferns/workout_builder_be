20960d7ceaba7ab1e398a78aef06758f2fc060a6
BF <burlferns@gamil.com>
2020-10-28 15:55:26 -0700

    made the coaches seed a bit more readable

--- data/seeds/01-coaches.js
+++ data/seeds/01-coaches.js
@@ -6,8 +6,18 @@ exports.seed = function(knex) {
     .truncate()
     .then(function () {
       return knex('coaches').insert([
-        {first_name: 'Arnold', last_name:'Swas', email:'as@mail.com', password: bcrypt.hashSync('qaz', hashRounds)},
-        {first_name: 'Charles', last_name:'Johnson', email:'cj@mail.com', password: bcrypt.hashSync('qaz', hashRounds)}
+        { 
+          first_name: 'Arnold', 
+          last_name:'Swas', 
+          email:'as@mail.com', 
+          password: bcrypt.hashSync('qaz', hashRounds)
+        },
+        {
+          first_name: 'Charles', 
+          last_name:'Johnson', 
+          email:'cj@mail.com', 
+          password: bcrypt.hashSync('qaz', hashRounds)
+        }
       ]);
     });
 };
