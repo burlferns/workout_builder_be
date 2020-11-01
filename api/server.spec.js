511c6a8b6ca25d289f3b5f2d91cdc9a88c787b9c
BF <burlferns@gamil.com>
2020-11-01 15:22:44 -0800

    cleaned server.spec.js

--- api/server.spec.js
+++ api/server.spec.js
@@ -13,16 +13,17 @@ describe('server is up check', () => {
       });
   });
 
-
-  //This afterAll is just there to access the database at the end so that 
-  //Jest does not end up with an open handle that prevents it from exiting
-  //The --detectOpenHandles CLI option detects this.
-  //Don't know why you need to access the database at the end to prevent this
-  //situation and I don't think it matters what you access in the database 
-  //The problem of open handles seems to occur when running the router tests
-  //that don't access the database directly
-  afterAll(async () => {
-    await db('exercises').where({coach_id:1000});
+  
+  //This is to enable Jest to exit properly
+  afterAll(function (done) {
+    server.close(done);
   });
+
+  //This is to enable Jest to exit properly
+  afterAll( (done) =>{
+    db.destroy(done);
+  })
+
+  
 });
 
